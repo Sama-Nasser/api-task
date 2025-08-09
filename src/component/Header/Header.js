c03b95657600a18fb5d75a39157187cfe925802c
Sama-Nasser <167590778+Sama-Nasser@users.noreply.github.com>
2025-08-10 00:13:48 +0300

    Header.js

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -1,1 +1,16 @@
- 
+export function Header() {
+    const header = document.createElement("header");
+    header.innerHTML = `
+        Time: <span id="clock"></span>
+    `;
+
+    const clockSpan = header.querySelector("#clock");
+    function updateClock() {
+        const now = new Date();
+        clockSpan.textContent = now.toLocaleTimeString();
+    }
+    setInterval(updateClock, 1000);
+    updateClock();
+
+    return header;
+}
