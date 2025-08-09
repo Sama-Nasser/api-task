a80851c46455b2090534331bbdf70fdd05c4c8ed
Sama-Nasser <167590778+Sama-Nasser@users.noreply.github.com>
2025-08-10 00:19:24 +0300

    IdSelect.js

--- src/component/GetForm/IdSelect.js
+++ src/component/GetForm/IdSelect.js
@@ -7,10 +7,11 @@ export function IdSelect() {
     select.disabled = true;
     select.innerHTML = `
         <option value="">--Select--</option>
-        ${Array.from({ length: 10 }, (_, i) => `<option value="${i + 1}">ID ${i + 1}</option>`).join("")}
+        ${Array.from({ length: 100 }, (_, i) => `<option value="${i + 1}">ID ${i + 1}</option>`).join("")}
     `;
 
     const container = document.createElement("div");
     container.append(label, select);
     return { container, select };
 }
+
